708a8660925961a468ac9fba9d76dc32794fcdee
zerojoon2 <comboy8231@gmail.com>
2024-11-17 13:58:37 +0900

    commit1 20241117

---
+++ config/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render, redirect
+
+def base(request):
+    return render(request, 'base.html')
+
+def index(request):
+    return render(request, 'index.html')
+
+def vision(request):
+    return render(request, 'vision.html')
+
+def demolition(request):
+    return render(request, 'demolition.html')
+
+def sanding(request):
+    return render(request, 'sanding.html')
